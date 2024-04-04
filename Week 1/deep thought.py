b7a622915b91ba09a98334fdd40db1ccd198b49e
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:40:29 +0200

    Update deep thought.py

--- Week 1/deep thought.py
+++ Week 1/deep thought.py
@@ -1,1 +1,10 @@
+# prompt user for input
+answer = input("What is the answer to the great question of life? ")
 
+# output Yes if input is 42, forty two, forty-two
+if answer =="42" or answer =="FoRty TwO" or answer =="forty two" or answer =="forty-two":
+    print("Yes")
+
+# output No if input is not 42
+else:
+    print("No")
