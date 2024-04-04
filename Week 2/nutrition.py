41834f53b462cdf692a4f4054129d178de5dd401
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:43:50 +0200

    Update nutrition.py

--- Week 2/nutrition.py
+++ Week 2/nutrition.py
@@ -1,1 +1,29 @@
-n
+
+fruits = {"apple":"130",
+         "Avocado":"50",
+         "banana":"110",
+         "cantaloupe":"50",
+         "grapefruit":"60",
+         "grapes":"90",
+         "honeydew melon":"50",
+         "Kiwifruit":"90",
+         "lemon":"15",
+         "lime":"20",
+         "nectarine":"60",
+         "orange":"80",
+         "peach":"60",
+         "pear":"100",
+         "pineapple":"50",
+         "plums":"70",
+         "strawberries":"50",
+         "Sweet Cherries":"100",
+         "tangerine":"50",
+         "watermelon":"80"}
+
+fruit = input("Item: ")
+
+for f in fruits:
+    if fruit in fruits:
+        print("Calories: ", fruits[fruit])
+        break
+
