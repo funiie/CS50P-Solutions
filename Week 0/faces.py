7f38dbec711cc797c7275bd3057c96ff22c21569
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 16:07:23 +0200

    Update faces.py

--- Week 0/faces.py
+++ Week 0/faces.py
@@ -1,1 +1,6 @@
-
+def convert():
+    # prompt user to enter text
+    text = str(input("Enter your text: "))
+    # convert frown emoticon to emoji
+    print(text.replace(":)", "🙂").replace(":(", "☹️" ))      
+convert()     
