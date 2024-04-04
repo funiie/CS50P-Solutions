d750177cebb31158c13f9baeae2a6274bb049dc8
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:42:46 +0200

    Update camel.py

--- Week 2/camel.py
+++ Week 2/camel.py
@@ -1,1 +1,11 @@
+def main():
+    # prompt user to input camelcase
+    camel = input("CamelCase: ")
 
+    # check and replace
+    for i in camel:
+        if i.isupper():
+            camel = camel.replace(i, "_" + i.lower())
+    print("snake name:", camel)
+
+main()
