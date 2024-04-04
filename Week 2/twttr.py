9ece6108c73c31c9c9620ab9018652956a6a2967
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:44:54 +0200

    Update twttr.py

--- Week 2/twttr.py
+++ Week 2/twttr.py
@@ -1,1 +1,10 @@
+#prompt user for input
+word = str(input("Input: "))
+vowels = ["A","E","I","O","U","a","e","i","o","u"]
 
+# out str with all vowels ommitted
+for v in vowels:
+    if v in word:
+        word = word.replace(v, "")
+
+print("output:", word)
