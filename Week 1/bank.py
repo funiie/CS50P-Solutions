67f3a539aa985a7e4bd22877b347f7a0f300d6a4
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:35:55 +0200

    Update bank.py

--- Week 1/bank.py
+++ Week 1/bank.py
@@ -1,1 +1,14 @@
+# prompts the user for greeting
+greeting = input("greet someone: ")
 
+# output $0 if greeting start with "hello"
+if greeting [:5] =="Hello" or greeting [:5] =="hello":
+    print("$0")
+
+# output $20 if greeting starts with "h" but not "hello"
+elif greeting [0] =="H" or greeting [0] =="h":
+    print("$20")
+
+# output $100 if neither
+else:
+    print("$100")
