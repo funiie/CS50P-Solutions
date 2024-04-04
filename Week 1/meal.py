8e985348ff81eed1df62b928e5c6c23a5ebe6d03
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2024-04-04 15:42:03 +0200

    Update meal.py

--- Week 1/meal.py
+++ Week 1/meal.py
@@ -1,1 +1,25 @@
 
+def main():
+    # prompt user to input time
+    time = input("What time is it? ")
+    time = convert(time)
+
+    # if statements
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+
+    if time >= 12 and time <= 13:
+       print("lunch time")
+
+    if time >= 18 and time <= 19:
+        print("dinner time")
+
+# function to convert str time to int and format time
+def convert(time):
+    hours, minutes = time.split(":")
+    hrs = float(hours)
+    mins = float(minutes) / 60
+    return float(hrs) + mins
+
+main()
+
