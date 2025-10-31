bb22d5feb2eb743a530316b5ae5a82f9b1f750c6
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2025-10-31 17:00:35 +0200

    Update Making faces.py

--- week0/Making faces.py
+++ week0/Making faces.py
@@ -1,1 +1,12 @@
+# Converts emoticons :) and :( to emoji
+
+# Prompt the user for input
+text = input("TYPE SOMETHING: ")
+
+# Replace :) with 🙂 and :( with 🙁
+text = text.replace(":)", "🙂")
+text = text.replace(":(", "🙁")
+
+# Print the result
+print(text)
 
