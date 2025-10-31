ee4e8f339a887120ef1387040fc9a9191cff99dc
Funiwe Nyawo <145009117+funiie@users.noreply.github.com>
2025-10-31 16:58:25 +0200

    Update Playback speed.py

--- week0/Playback speed.py
+++ week0/Playback speed.py
@@ -1,1 +1,8 @@
+# Slow down speech by replacing spaces with '...'
+
+# Prompt the user for input
+text = input("Type something: ")
+
+# Replace spaces with '...' and print the result
+print(text.replace(" ", "..."))
 
